4e0e0ea1e7cfff70fecd94b0b313b5600da55fde
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-03-30 21:39:41 -0300

    Adicionando a query de paises

---
+++ Paises_03/queryPaises.sql
@@ -0,0 +1,25 @@
+create database SistemaPaises;
+use SistemaPaises;
+
+create table paises(
+id int auto_increment primary key,
+nome varchar (100), 
+populacao bigint,
+area numeric (15,2)
+);
+
+SELECT id, nome, populacao, area FROM paises WHERE id = 4;
+
+select * from paises;
+
+delete from paises where id > 6;
+
+insert into paises(nome, populacao, area) values ("Italia", 300000, 200.20);
+insert into paises(nome, populacao, area) values ("Japão", 300000, 200.20);
+
+Select id, nome, populacao, area FROM paises order by populacao desc limit 1;
+
+Select id, nome, populacao, area FROM paises order by area asc limit 1;
+
+SELECT * FROM paises WHERE paises.id = 1 or paises.id = 2 or paises.id = 3;
+
