d61971a181b0724a2da9098ea5b9677cb2c33bc1
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-05-11 22:26:00 -0300

    feature: desenvolvendo o filtro de log

--- Paises_03/src/filter/LogFilter.java
+++ Paises_03/src/filter/LogFilter.java
@@ -1,19 +1,30 @@
 package filter;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Paths;
+import java.util.Calendar;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+
 import model.Usuario;
+import utils.Log;
 
 @WebFilter("/*")
 public class LogFilter implements Filter {
+	FilterConfig filterConfig = null;
+
 	public void destroy() {
 	}
 
@@ -24,19 +35,33 @@ public class LogFilter implements Filter {
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpSession session = req.getSession();
 		Usuario usuario = (Usuario) session.getAttribute("logado");
+		String comando = req.getParameter("command");
+		if (comando == null) {
+			comando = req.getRequestURI();
+		}
+		Calendar timestamp = Calendar.getInstance();
+		String textoLog = "";
+		ServletContext servletContext = filterConfig.getServletContext();
+		String contextPath = servletContext.getRealPath(File.separator);
 		if (usuario == null) {
-			System.out.println(req.getParameter("command"));
+			textoLog = String.format("[%1$tA, %1$tB %1$td, %1$tY %1$tZ %1$tI:%1$tM:%1$tS:%1$tL %tp] %s\n", timestamp, comando);
+
 		} else {
-			System.out.println(usuario.getUsername() + " -> " +	req.getParameter("command"));
+			textoLog = String.format("[%1$tA, %1$tB %1$td, %1$tY %1$tZ %1$tI:%1$tM:%1$tS:%1$tL %tp] %s -> %s\n", timestamp, usuario.getUsername(), comando);
+		}
+		String logDirectory = contextPath + File.separator + "log";
+		if (!Files.exists(Paths.get(logDirectory), LinkOption.NOFOLLOW_LINKS)) new File(logDirectory).mkdir();
+		
+		synchronized (textoLog) {
+			Log arqLog = new Log();
+			arqLog.abrir(contextPath + File.separator + "log" + File.separator + Log.NOME);
+			arqLog.escrever(textoLog);
+			arqLog.fechar();
 		}
 		chain.doFilter(request, response);
-		if (usuario == null) {
-			System.out.println(req.getParameter("command"));
-		} else {
-			System.out.println(req.getParameter("command") + " -> " + usuario.getUsername());
-		}
 	}
 
 	public void init(FilterConfig fConfig) throws ServletException {
+		this.filterConfig = fConfig;
 	}
 }
