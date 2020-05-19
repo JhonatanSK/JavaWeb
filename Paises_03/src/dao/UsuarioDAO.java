5b0b6f7ff473996e6be817a8ee9ed09fcbd7ba63
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-05-19 01:57:06 -0300

    feature: desenvolvendo a dao para cadastrar usuários

--- Paises_03/src/dao/UsuarioDAO.java
+++ Paises_03/src/dao/UsuarioDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 import model.Usuario;
 
 public class UsuarioDAO {
@@ -31,4 +32,30 @@ public class UsuarioDAO {
 		}
 		return false;
 	}
+	
+	public int cadastrar(Usuario to) throws Exception {
+		int id = 0;
+		String sqlInsert = "INSERT INTO usuario (username, password) values (?,?) ;";
+		
+		try (Connection conn = ConnectionFactory.obtemConexao();
+			 PreparedStatement stm = conn.prepareStatement(sqlInsert)) {
+			stm.setString(1, to.getUsername());
+			stm.setString(2, to.getPassword());
+							
+			stm.execute();
+			
+			try (ResultSet rs = stm.executeQuery("SELECT LAST_INSERT_ID()")) {
+				if (rs.next()) {				
+					id = rs.getInt(1);
+				}
+			} catch (SQLException ex) {
+				throw new Exception(ex.getMessage());
+			}
+		} catch (SQLException e) {
+			throw new Exception(e.getMessage());
+		}
+		
+		return id;
+	}
+	
 }
